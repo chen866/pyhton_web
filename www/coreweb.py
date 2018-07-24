153f66f72212237ec58bd24d067bc3740a3790b9
chen866 <1617383757@qq.com>
2018-07-24 23:32:14 +0800

    未完成

--- www/coreweb.py
+++ www/coreweb.py
@@ -64,11 +64,13 @@ def has_request_args(fn):
     params = sig.parameters
     found = False
     for name, param in params.items():
-        if name == 'request' :
+        if name == 'request':
             found = Ture
             continue
-        if fonnd and (param.kind != inspect.Parameter.VAR_POSITIONAL and param.kind != inspect.Parameter.KEYWORD_ONLY and param.kind != inspect.Parameter.VAR_KEYWORD):
-            raise ValueError('request parameter must be the last named parameter in function: %s%s' % (fn.__name__, str(sig)))
+        if fonnd and (
+                param.kind != inspect.Parameter.VAR_POSITIONAL and param.kind != inspect.Parameter.KEYWORD_ONLY and param.kind != inspect.Parameter.VAR_KEYWORD):
+            raise ValueError(
+                'request parameter must be the last named parameter in function: %s%s' % (fn.__name__, str(sig)))
     return found
 
 
@@ -79,5 +81,30 @@ class RequestHandler(object):
         self._has_request_args = has_request_args(fn)
         self._has_var_kw_args = has_var_kwargs(fn)
         self._named_kw_args = get_named_kwargs(fn)
-        self.required_kw_args = get_required_kwargs(fn)
-        # TODO
+        self._required_kw_args = get_required_kwargs(fn)
+
+        @asyncio.coroutine
+        def __call__(self, request):
+            kw = None
+            if self._has_var_kw_args or self._has_var_kw_args or self._required_kw_args:
+                if request.method == 'POST':
+                    if not request.content_type:
+                        return web.HTTPBadRequest('Missing Content-Type.')
+                    ct = request.content_type.lower()
+                    if ct.startswith('application/json'):
+                        params = yield from request.json()
+                        if not isinstance(params, dict):
+                            return web.HTTPBadRequest('JSON body must be object.')
+                        kw = params
+                    elif ct.startswith('application/x-www-form-urlencoded') or ct.startswith('multipart/form-data'):
+                        params = yield from request.post()
+                        kw = dict(**params)
+                    else:
+                        return web.HTTPBadRequest('Unsupported Content-Type: %s' % request.content_type)
+                if request.method == 'GET':
+                    qs = request.query_string
+                    if qs:
+                        kw = dict()
+                        for k, v in parse.parse_qs(qs, True).items():
+                            kw[k] = v[0]
+                            # TODO
