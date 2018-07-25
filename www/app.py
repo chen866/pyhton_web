4d90da8f064170d9dcf412713771882d8a2d9225
chen866 <1617383757@qq.com>
2018-07-25 23:31:43 +0800

    有空接着写

--- www/app.py
+++ www/app.py
@@ -1,9 +1,47 @@
 import logging
-import asyncio
+import asyncio, os, json, time
+from datetime import datetime
 
 from aiohttp import web
+from jinja2 import Environment, FileSystemLoader
+import orm
+from coreweb import add_routers, add_static
 
 logging.basicConfig(level=logging.INFO)
+
+
+def init_jinja2(app, **kwargs):
+    logging.info('init jinja2...')
+    options = dict(
+        autoescape=kwargs.get('autoescape', True),
+        block_start_string=kwargs.get('block_start_string', '{%'),
+        block_end_string=kwargs.get('block_end_string', '%}'),
+        variable_start_string=kwargs.get('variable_start_string', '{{'),
+        variable_end_string=kwargs.get('variable_end_string', '}}'),
+        auto_reload=kwargs.get('auto_reload', True)
+    )
+
+    path = kwargs.get('path', None)
+    if path is None:
+        Path = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'templates')
+    logging.info('set jinja2 template path: %s' % path)
+    env = Environment(loader=FileSystemLoader(path), **options)
+    filters = kwargs.get('filters', None)
+    if filters is not None:
+        for name, f in filters.items():
+            env.filters[name] = f
+    app['__templating__'] = env
+
+
+async def logger_factory(app, handler):
+    async def logger(request):
+        logging.info('Request: %s %s' % (request.method, request.path))
+        # await asyncio.sleep(0.3)
+        return (await handler(request))
+    return parse_data
+
+
+# TODO
 
 
 def index(request):
@@ -12,10 +50,10 @@ def index(request):
 
 # Web App骨架
 @asyncio.coroutine
-def init(Loop):
+async def init(Loop):
     app = web.Application(loop=Loop)
     app.router.add_route('GET', '/', index)
-    server = yield from Loop.create_server(app.make_handler(), '127.0.0.1', '9000')
+    server = await Loop.create_server(app.make_handler(), '127.0.0.1', '9000')
     logging.info('server started at http://127.0.0.1:9000...')
     return server
 
