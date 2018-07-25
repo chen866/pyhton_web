76933a1e54a0f49ca0f2842a5e6efdb72db68e73
chen866 <1617383757@qq.com>
2018-07-25 22:17:44 +0800

    添加单个配置文件，但未进行读取

---
+++ www/config_default.py
@@ -0,0 +1,12 @@
+configs = {
+    'db': {
+        'host': '127.0.0.1',
+        'port': '3306',
+        'user': 'root',
+        'password': 'root',
+        'database': 'awesome2'
+    },
+    'session': {
+        'secret': 'Awesome'
+    }
+}
