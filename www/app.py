4eba607c3c7b2c666b7910bba2e83b8d377b7ca4
chen866 <1617383757@qq.com>
2018-07-27 22:51:55 +0800

    app.py

--- www/app.py
+++ www/app.py
@@ -41,22 +41,81 @@ async def logger_factory(app, handler):
     return parse_data
 
 
-# TODO
+async def data_factory(app, handle):
+    async def parse_data(request):
+        if request.method == 'POST':
+            if request.content_type.startswith('application/json'):
+                request.__data__ = await request.json()
+                logging.info('request json: %s' % str(request.__data__))
+            elif request.content_type.startswith('application/x-www-form-urlencoded'):
+                request.__data__ = await request.post()
+                logging.info('request from: %s' % str(request.__data__))
+        return (await handler(request))
+    return parse_data
 
 
-def index(request):
-    return web.Response(body=b'<h1>This is the first page!</h1>', content_type='text/html')
+async def reponse_factory(app, handle):
+    async def response(request):
+        logging.info('Response handler...')
+        r = await handler(request)
+        if isinstance(r, web.StreamPesponse):
+            return r
+        elif isinstance(r, bytes):
+            resp = web.Response(body=r)
+            resp.content_type = 'application/octet-stream'
+            return resp
+        elif isinstance(r, str):
+            if r.startswith('redirect:'):
+                return web.HTTPFound(r[9:])
+            resp = web.Response(body=r.encode('utf-8'))
+            resp.content_type = 'text/hrml;chartset=utf-8'
+            return resp
+        elif isinstance(r, dict):
+            template = r.get('__template__')
+            if template is None:
+                resp = web.Response(body=json.dumps(r, ensure_ascii=False, default=lambda o: o.__dict__).encode('utf-8'))
+                resp.content_type = 'application/json;charset=utf-8'
+                return resp
+            else:
+                resp = web.Response(body=app['__templating__'].get_template(template).render(**r).encode('utf-8'))
+                resp.content_type = 'text/html;chartset=utf-8'
+                return resp
+        elif isinstance(r, int) and 100 <= r < 600:
+            return web.Response(t, str(m))
+        elif isinstance(r, tuple) and len(r) == 2:
+            t, m = r
+            if isinstance(t, int) and 100 < t < 600:
+                return web.Response(t, str(m))
+        # default
+        resp = web.Response(body=str(r).encode('utf-8'))
+        resp.content_type = 'text/plain;charset=utf-8'
+        return resp
+    return response
 
 
-# Web App骨架
-@asyncio.coroutine
+def datetime_filter(t):
+    delta = int(time.time() - t)
+    if delta < 60:
+        return u'一分钟前'
+    elif delta < 3600:
+        return u'%s分钟前' % (delta // 60)
+    elif delta < 86400:
+        return u'%s小时前' % (delta // 3600)
+    elif delta < 604800:
+        return u'%s天前' % (delta // 86400)
+    dt = datatime.formtimestamp(t)
+    return u'%s年%s月%s日' %s (dt.year, dt.mouth, dt.day)
+
+
 async def init(Loop):
-    app = web.Application(loop=Loop)
-    app.router.add_route('GET', '/', index)
-    server = await Loop.create_server(app.make_handler(), '127.0.0.1', '9000')
+    await orm.create_pool(loop=Loop, host='127.0.0.1', post='3306', user='root', password='root', db='awesome')
+    app = web.Application(loop=Loop, middlewares=[logger_factory, reponse_factory])
+    init_jinja2(app, filters=dict(datatime_filter))
+    add_routers(app, 'handlers')
+    add_static(app)
+    srv = await loop.create_server(app.make_handler(), '127.0.0.1', 9000)
     logging.info('server started at http://127.0.0.1:9000...')
-    return server
-
+    return srv
 
 loop = asyncio.get_event_loop()
 loop.run_until_complete(init(loop))
