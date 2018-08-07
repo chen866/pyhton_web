52c4c5d89d68fa783057dcf00327f38b2f655e72
chen866 <1617383757@qq.com>
2018-08-07 22:32:22 +0800

    handlers

--- www/handlers.py
+++ www/handlers.py
@@ -7,8 +7,21 @@ from models import User, Comment, Blog, next_id
 
 @get('/')
 async def index(request):
-    users = await User.findAll()
+    summary = 'login ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magne qliqa.'
+    blogs = [
+        Blog(id='1', name='Test Blog 1', summary=summary, create_time=time.time() - 120),
+        Blog(id='2', name='Test Blog 3', summary=summary, create_time=time.time() - 3600),
+        Blog(id='3', name='Test Blog 3', summary=summary, create_time=time.time() - 7200)
+    ]
     return {
-        '__templates__': 'test.html',
-        'users': users
+        '__templates__': 'blogs.html',
+        'blogs': blogs
     }
+
+
+@get('/api/users')
+async def api_get_users():
+    users = await User.findAll(orderBy='create_time desc')
+    for u in users:
+        u.password = '******'
+    return dict(users=users)
